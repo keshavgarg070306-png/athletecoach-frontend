c9df39d339a3ce625c290be3772e25bd35d4209c
Keshav Garg <keshavgarg070306@gmail.com>
2026-03-22 14:50:45 +0530

    Update API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:8080/api'
+  baseURL: process.env.NODE_ENV==='production'
+    ? 'https://web-production-14265.up.railway.app/api'
+    : 'http://localhost:8080/api'
 })
 
 export const login = (data) => API.post('/auth/login', data)
